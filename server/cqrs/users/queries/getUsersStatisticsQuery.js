e5c0c07c8adb13653a72ae96d4bb22459efb06c9
Gilles Perreymond <gperreymond@gmail.com>
2017-11-07 11:12:49 +0100

    getUsersStatisticsQuery based on collector data done

--- server/cqrs/users/queries/getUsersStatisticsQuery.js
+++ server/cqrs/users/queries/getUsersStatisticsQuery.js
@@ -1,16 +1,28 @@
+const path = require('path')
+const Promise = require('bluebird')
+const glob = require('glob-promise')
+
 const method = function (next) {
   const start = new Date()
-  next(null, {
-    type: 'query',
-    domain: 'users',
-    name: 'getUsersStatisticsQuery',
-    exectime: new Date() - start,
-    result: {
-      admin: 3,
-      users: 2345,
-      lasts: 34
-    }
-  })
+  const usersDirpath = path.resolve(__dirname, '../../../../data/collector/postgres/individuals')
+  const lastsDirpath = path.resolve(usersDirpath, '2017/06')
+  let requests = {
+    users: glob(path.resolve(usersDirpath, '**/*.yml')),
+    lasts: glob(path.resolve(lastsDirpath, '**/*.yml'))
+  }
+  Promise.props(requests).then(result => {
+    next(null, {
+      type: 'query',
+      domain: 'users',
+      name: 'getUsersStatisticsQuery',
+      exectime: new Date() - start,
+      result: {
+        admin: 3,
+        users: result.users.length,
+        lasts: result.lasts.length
+      }
+    })
+  }).catch(next)
 }
 
 module.exports = method
